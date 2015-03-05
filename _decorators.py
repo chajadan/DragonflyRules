1847c03925ae6ee47596bb6c429b54eb1f509c5e
Charles J. Daniels <chajadan@fake.chajadan>
2015-03-04 22:44:49 -0800

    redesign BombRule to manually find first instance of the word bomb

--- _decorators.py
+++ _decorators.py
@@ -3,16 +3,34 @@
 #    ??.add_rule(RuleClass())
 
 from dragonfly import *
+from dragonfly.engines.backend_natlink.dictation import NatlinkDictationContainer
 
 #decorator
 def BombRule(CompoundRuleClass):
-    CompoundRuleClass.spec += " [bomb [<chain>]]"
-    CompoundRuleClass.extras += (Dictation("chain"),)
     _orig_process_recognition = CompoundRuleClass._process_recognition
     def _new_process_recognition(self, node, extras):
+        words = []
+        bombCount = 0
+        
+        #seek out bomb
+        for name, value in extras.items():
+            if type(value) == NatlinkDictationContainer:
+                if value.words.count("bomb") == 0:
+                    continue # try to find another dictation container with a bomb
+                else:
+                    words = value.words
+                    bombCount = words.count("bomb")
+                    break
+                
+        if bombCount:
+            bombIndex = words.index("bomb")
+            extras[name] = NatlinkDictationContainer(words[0:bombIndex])
+            
         _orig_process_recognition(self, node, extras)
-        if extras.has_key("chain"):
-            Mimic(*extras["chain"].words).execute()
+        
+        if bombCount:
+            Mimic(*words[bombIndex + 1:]).execute()
+            
     CompoundRuleClass._process_recognition = _new_process_recognition
     return CompoundRuleClass
 
