0502d14a0f6ae6d1466129e5246b62086950e527
Charles J. Daniels <chajadan@fake.chajadan>
2015-03-08 12:07:16 -0700

    Add safeguard to deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,14 @@
+REM the following line is a safeguard to protect others from accidental directory content deletion
+
+if %COMPUTERNAME% == CHAJDESKTOPWIN78 (
+
 del /Q C:\NatLink\NatLink\MacroSystem
 xcopy . C:\NatLink\NatLink\MacroSystem /Y /EXCLUDE:deployExclude.txt
+
+)
+
+ELSE (
+
+echo This file needs to be edited to remove/address the file system deletion safeguard
+
+)
