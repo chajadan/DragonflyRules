8d4958492ddec3aeb40ce371098386b579da29d0
Charles J. Daniels <chajadan@fake.chajadan>
2015-05-13 23:53:16 -0700

    Add "do git clean" to git bash rules, = "git clean" Add "do force git clean" to git bash rules = "git clean -f" Add system specific browse to local repository rules in git bash rules. Add "add updated" to git bash rules, = "git add -u"

--- sh.py
+++ sh.py
@@ -1,24 +1,29 @@
 from dragonfly import *
-import BaseGrammars
-from BaseRules  import *
+import Base
 
 grammar_context = AppContext(executable = "sh")
-grammar = BaseGrammars.GlobalGrammar("sh", context = grammar_context)
+grammar = Base.ContinuousGrammar("sh", context = grammar_context)
 
 
-class GitBashRules(QuickRules):
+class GitBashRules(Base.QuickContinuousRules):
     context= AppContext(title = "MINGW32"),
     name="GitBashRules"
     mapping = {
-         "add all": Text("git add .") + Key("enter"),
-         "add all with deletes": Text("git add -A") + Key("enter"),
-         "status": Text("git status") + Key("enter"),
-         "diff": Text("git diff") + Key("enter"),
-         "commit": Text("git commit"),
-         "commit [with] message <text>": {
+        "add all": Text("git add .") + Key("enter"),
+        "add all with deletes": Text("git add -A") + Key("enter"),
+        "add updated": Text("git add -u") + Key("enter"),
+        "status": Text("git status") + Key("enter"),
+        "diff": Text("git diff") + Key("enter"),
+        "commit": Text("git commit"),
+        "commit [with] message <text>": {
             "action": Text('git commit -m "%(text)s"'),
             "intro": ["commit message", "commit with message"]},
-         "push": Text("git push") + Key("enter"),
+        "push": Text("git push") + Key("enter"),
+        "go to ACI Compiler": Text("cd ~/git/AciCompiler/AciCompiler/AciCompiler") + Key("enter"),
+        "go to AciImporter": Text("cd ~/git/AciImporter") + Key("enter"),
+        "go to dragonfly rules": Text("cd ~/git/DragonflyRules/DragonflyRules/src") + Key("enter"),
+        "do git clean": Text("git clean") + Key("enter"),
+        "do force git clean": Text("git clean -f") + Key("enter"),
     }
     extrasDict = {
         "text": Dictation("text"),
