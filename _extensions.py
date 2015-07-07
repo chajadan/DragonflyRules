5b2d81dfdb28d7915b3b3b4d428188afc7d416bb
Charles J. Daniels (chajadan) <git@chajadan.net>
2015-07-06 17:14:52 -0700

    Transfrom into QuickContinuousRules Add optional [with|give] to extension <extension> command Add "[the] extension [for] <extension>" command

--- _extensions.py
+++ _extensions.py
@@ -1,31 +1,25 @@
 from dragonfly import *
 import Base
 import inspect
+from decorators import ActiveGrammarRule
 
 grammar = Base.ContinuousGrammar("file extensions grammar")
-
-#decorator
-def GrammarRule(rule):
-    if inspect.isclass(rule):
-        if issubclass(rule, Base.BaseQuickRules):
-            rule(grammar)
-        else:
-            grammar.add_rule(rule())
-    else:
-        grammar.add_rule(rule)
-               
-@GrammarRule
-class PrefixedKeypressRule(Base.ContinuousRule):
-    spec = "extension <ext>"
+              
+@ActiveGrammarRule(grammar)
+class PrefixedKeypressRule(Base.QuickContinuousRules):
     extensions = {
         "Python": "py",
         "text": "txt",
         "see plus plus": "cpp",
         "header file": "h",
+    }    
+    mapping = {
+        "[with|give] extension <extension>": Text(".%(extension)s"),
+        "[the] extension [for] <extension>": Text(".%(extension)s"),
     }
-    extras = (Choice("ext", {name: extension for name, extension in extensions.items()}),)
-    def _process_recognition(self, node, extras):
-        Text("." + extras["ext"]).execute()
+    extrasDict = {
+        "extension": Choice("extension", {name: extension for name, extension in extensions.items()})
+    }
 
 grammar.load()
 def unload():
